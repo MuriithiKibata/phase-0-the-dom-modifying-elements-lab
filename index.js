6a90262255fb6ed62981d250ccb8cba8053904d9
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-09 21:51:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const x = document.getElementById("main") 
+
+  
+    x.remove(main)
+   // const newHeader = document.body.append(<h1 id ="victory"></h1>)
+var newHeader = document.createElement("h1")
+newHeader.setAttribute('id','victory')
+newHeader.textContent += "YOUR-NAME is the champion"
